3da17278a6f30ba159f9b7b0e275ab334d96a745
jacobofernandez420 <j.kozlowski420@interia.pl>
2023-04-24 15:52:33 +0200

    konsumowanie danych z zewnetrznych api

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Test_web_app.Services.Interfaces;
+
+namespace Test_web_app.Controllers
+{
+    [Authorize]
+    public class ApiController : Controller
+    {
+        private readonly IApiService _apiService;
+
+        public ApiController(IApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public IActionResult Index()
+        {
+            var response = _apiService.Get("london");
+            return View(response);
+        }
+    }
+}
---
+++ Services/ApiService.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using Test_web_app.Models;
+using Test_web_app.Services.Interfaces;
+
+namespace Test_web_app.Services
+{
+    public class ApiService : IApiService
+    {
+        private const string API_KEY = "ceca0dfec12ee134d93c97f0a45b0d6a";
+
+        public WeatherResponse Get(string city)
+        {
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}";
+
+            var web = new WebClient();
+
+            var response = web.DownloadString(url);
+
+            WeatherResponse myDeserializedClass = JsonConvert.DeserializeObject<WeatherResponse>(response);
+
+            myDeserializedClass.main.temp_max = myDeserializedClass.main.temp_max - 273.15;
+            myDeserializedClass.main.temp_min = myDeserializedClass.main.temp_min - 273.15;
+
+            return myDeserializedClass;
+
+        }
+    }
+}
